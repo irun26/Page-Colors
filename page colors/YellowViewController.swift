32d5c3762f2944b8e8f7bfd96cd3edef0d865cae
Jimson Vedua <jmsn133@gmail.com>
2019-02-20 22:36:24 -0500

    2nd commit get yellow page in there

---
+++ page colors/YellowViewController.swift
@@ -0,0 +1,31 @@
+//
+//  YellowViewController.swift
+//  page colors
+//
+//  Created by Jimson Vedua on 2/20/19.
+//  Copyright © 2019 SameTeam. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("yellow page")
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
